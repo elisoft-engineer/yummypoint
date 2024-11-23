bcbc29865f496dae9d801dc2f7c3cdd87d7c5580
Elkana Maina <elisoft.engineer@gmail.com>
2024-11-23 08:40:35 +0300

    Remove admin related routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,10 +7,6 @@ urlpatterns = [
     path('signin/', views.CustomerSignin.as_view(), name='signin'),
     path('topup/', views.Topup.as_view(), name='topup'),
 
-    # admin auth routes
-    path('signup/admin/', views.AdminSignup.as_view(), name='admin-signup'),
-    path('signin/admin/', views.AdminSignin.as_view(), name='admin-signin'),
-
     # common auth routes
     path('me/', views.Account.as_view(), name="account"),
     path('signout/', views.Signout.as_view(), name='signout'),
