67647bb22defdc9c9ba847071210bac0ee2d4cec
Elkana Maina <elisoft.engineer@gmail.com>
2024-11-23 08:42:37 +0300

    Change from admin routes to panel routes

--- core/urls.py
+++ core/urls.py
@@ -6,13 +6,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     #admin urls
-    path('admin/', admin.site.urls),
+    path('admin/', include('panel.urls')),
     #core urls
     path('', core_views.Index.as_view(), name="index"),
     path('about/', core_views.About.as_view(), name="about"),
     path('contact/', core_views.Contact.as_view(), name="contact"),
     path('set_theme/<str:theme>/', core_views.set_theme, name='set_theme'),
-    path('admin-dashboard/', core_views.AdminDashboard.as_view(), name="admin-dashboard"),
     #app urls
     path('accounts/', include('accounts.urls')),
     path('menu/', include('menu.urls')),
