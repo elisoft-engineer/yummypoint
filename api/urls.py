b5ea18d8deb1582af20268abdc6a527b9ff05732
Elkana Maina <elisoft.engineer@gmail.com>
2024-11-25 10:35:10 +0300

    Add account-related routes

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # accounts
+    path('customers/', views.CustomerList.as_view(), name="api-customer-list"),
+    path('customers/<uuid:pk>/', views.CustomerDetail.as_view(), name="api-customer-detail"),
+    path('admins/', views.AdminList.as_view(), name="api-admin-list"),
+    path('admins/me/', views.AdminDetail.as_view(), name="api-admin-detail"),
 ]
