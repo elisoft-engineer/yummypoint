e59a2af0268510e2c071c81a280c55366f73c1df
Elkana Maina <elisoft.engineer@gmail.com>
2024-11-26 12:36:34 +0300

    Add inventory and feedback related routes

--- api/urls.py
+++ api/urls.py
@@ -13,4 +13,13 @@ urlpatterns = [
     path('customers/<uuid:pk>/', views.CustomerDetail.as_view(), name="api-customer-detail"),
     path('admins/', views.AdminList.as_view(), name="api-admin-list"),
     path('admins/me/', views.AdminDetail.as_view(), name="api-admin-detail"),
+    # messages
+    path('messages/', views.MessageList.as_view(), name="api-message-list"),
+    path('messages/<uuid:pk>/', views.MessageDetail.as_view(), name="api-message-detail"),
+    # inventory
+    path('inventory/', views.InventoryList.as_view(), name="api-inventory-list"),
+    path('inventory/<uuid:pk>/', views.InventoryDetail.as_view(), name="api-inventory-detail"),
+    # suppliers
+    path('suppliers/', views.SupplierList.as_view(), name="api-supplier-list"),
+    path('suppliers/<uuid:pk>/', views.SupplierDetail.as_view(), name="api-supplier-detail"),
 ]
