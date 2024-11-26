273822690fb9e85f166015b8915543de7a2c9dc8
Elkana Maina <elisoft.engineer@gmail.com>
2024-11-26 12:36:42 +0300

    Add inventory and feedback related views

--- api/views.py
+++ api/views.py
@@ -1,22 +1,20 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import CustomTokenObtainPairSerializer
 from django.shortcuts import get_object_or_404
-from django.db.models import Q
 from django.contrib.auth.models import Group
 from rest_framework_simplejwt.views import TokenObtainPairView
-from notifications.models import Notification, NotificationStatus
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.exceptions import PermissionDenied, ValidationError
 from .serializers import CustomTokenObtainPairSerializer, CustomerSerializer, CustomerCreateSerializer, \
-    AdminSerializer, AdminUpdateSerializer, AdminCreateSerializer, CustomerUpdateSerializer
-from .permissions import IsAdmin, IsCustomer, IsCustomerOrAdmin
+    AdminSerializer, AdminUpdateSerializer, AdminCreateSerializer, CustomerUpdateSerializer, \
+    MessageCreateSerializer, MessageSerializer, InventoryCreateSerializer, InventorySerializer, \
+    InventoryUpdateSerializer, SupplierSerializer, SupplierCreateSerializer, SupplierUpdateSerializer
+from feedback.models import MessageStatus, ContactMessage
+from .permissions import IsAdmin, IsCustomerOrAdmin
 from accounts.models import Customer, Admin
 from notifications.utils import create_notification
-from django.urls import reverse
+from inventory.models import Inventory, Supplier
 
 
 """
@@ -219,3 +217,237 @@ class AdminDetail(APIView):
         admin = self.get_object(self.request.user.id)
         admin.delete()
         return Response({"detail": "Admin deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+    
+
+class MessageList(APIView):
+    """
+    Endpoint to handle both fetching all messages and sending a new message.
+    """
+
+    allowed_methods = ['GET', 'POST']
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticated(), IsAdmin()]
+        return [AllowAny()]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return MessageCreateSerializer
+        return MessageSerializer
+
+    def get(self, request):
+        """
+        Handle GET request to fetch all messages.
+        """
+        read = request.query_params.get('read', None)
+        if read is not None:
+            read = read.lower() in ['true', '1', 'yes']
+            messages = ContactMessage.objects.filter(
+                status=MessageStatus.READ if read else MessageStatus.UNREAD,
+            )
+        else:
+            messages = ContactMessage.objects.all()
+        serializer = self.get_serializer_class()(messages, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """
+        Handle POST request to submit a message.
+        """
+        serializer = self.get_serializer_class()(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MessageDetail(APIView):
+    """
+    Retrieve or delete a message instance.
+    """
+
+    allowed_methods = ['GET', 'PATCH', 'DELETE']
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+    def get_serializer_class(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return None
+        return MessageSerializer
+
+    def get_object(self, pk):
+        return get_object_or_404(ContactMessage, pk=pk)
+
+    def get(self, request, pk):
+        """
+        Retrieve message details.
+        """
+        message = self.get_object(pk)
+        serializer = self.get_serializer_class()(message)
+        return Response(serializer.data)
+
+    def patch(self, request, pk):
+        message = self.get_object(pk)
+        message.status = MessageStatus.READ
+        message.save()
+        return Response({"detail": "Message marked as read"}, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        """
+        Delete a message.
+        """
+        message = self.get_object(pk)
+        message.delete()
+        return Response({"detail": "Message deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+    
+
+class InventoryList(APIView):
+    """
+    Endpoint to handle both fetching all inventory records and adding a new one.
+    """
+
+    allowed_methods = ['GET', 'POST']
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return InventoryCreateSerializer
+        return InventorySerializer
+
+    def get(self, request):
+        """
+        Handle GET request to fetch all records.
+        """
+        
+        records = Inventory.objects.all()
+        serializer = self.get_serializer_class()(records, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """
+        Handle POST request to add a new record.
+        """
+        serializer = self.get_serializer_class()(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class InventoryDetail(APIView):
+    """
+    Retrieve or delete a record.
+    """
+
+    allowed_methods = ['GET', 'PUT', 'DELETE']
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+    def get_serializer_class(self):
+        if self.request.method == 'DELETE':
+            return None
+        elif self.request.method == 'PUT':
+            return InventoryUpdateSerializer
+        return InventorySerializer
+
+    def get_object(self, pk):
+        return get_object_or_404(Inventory, pk=pk)
+
+    def get(self, request, pk):
+        """
+        Retrieve record details.
+        """
+        record = self.get_object(pk)
+        serializer = self.get_serializer_class()(record)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        record = self.get_object(pk)
+        serializer = self.get_serializer_class()(record, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        """
+        Delete a record.
+        """
+        record = self.get_object(pk)
+        record.delete()
+        return Response({"detail": "Inventory record deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+    
+
+class SupplierList(APIView):
+    """
+    Endpoint to handle both fetching all suppliers and adding a new one.
+    """
+
+    allowed_methods = ['GET', 'POST']
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return SupplierCreateSerializer
+        return SupplierSerializer
+
+    def get(self, request):
+        """
+        Handle GET request to fetch all suppliers.
+        """
+        
+        suppliers = Supplier.objects.all()
+        serializer = self.get_serializer_class()(suppliers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """
+        Handle POST request to add a new supplier.
+        """
+        serializer = self.get_serializer_class()(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SupplierDetail(APIView):
+    """
+    Retrieve, update or delete a supplier.
+    """
+
+    allowed_methods = ['GET', 'PUT', 'DELETE']
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+    def get_serializer_class(self):
+        if self.request.method == 'DELETE':
+            return None
+        elif self.request.method == 'PUT':
+            return SupplierUpdateSerializer
+        return SupplierSerializer
+
+    def get_object(self, pk):
+        return get_object_or_404(Supplier, pk=pk)
+
+    def get(self, request, pk):
+        """
+        Retrieve supplier details.
+        """
+        supplier = self.get_object(pk)
+        serializer = self.get_serializer_class()(supplier)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        supplier = self.get_object(pk)
+        serializer = self.get_serializer_class()(supplier, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        """
+        Delete a supplier.
+        """
+        supplier = self.get_object(pk)
+        supplier.delete()
+        return Response({"detail": "Supplier deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
