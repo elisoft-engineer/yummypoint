c9a2a98e9f2d5cd934e28698dd249b7bd9b1070b
Elkana Maina <elisoft.engineer@gmail.com>
2024-11-29 12:49:18 +0300

    Add Menu, Category and Review views

--- api/views.py
+++ api/views.py
@@ -9,12 +9,18 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from .serializers import CustomTokenObtainPairSerializer, CustomerSerializer, CustomerCreateSerializer, \
     AdminSerializer, AdminUpdateSerializer, AdminCreateSerializer, CustomerUpdateSerializer, \
     MessageCreateSerializer, MessageSerializer, InventoryCreateSerializer, InventorySerializer, \
-    InventoryUpdateSerializer, SupplierSerializer, SupplierCreateSerializer, SupplierUpdateSerializer
+    InventoryUpdateSerializer, SupplierSerializer, SupplierCreateSerializer, SupplierUpdateSerializer, \
+    CategorySerializer, CategoryCreateSerializer, MenuSerializer, MenuCreateSerializer, \
+    MenuUpdateSerializer, ReviewSerializer, ReviewCreateSerializer, ReviewUpdateSerializer
 from feedback.models import MessageStatus, ContactMessage
-from .permissions import IsAdmin, IsCustomerOrAdmin
+from .permissions import IsAdmin, IsCustomerOrAdmin, IsCustomer
 from accounts.models import Customer, Admin
 from notifications.utils import create_notification
 from inventory.models import Inventory, Supplier
+from menu.models import Menu, Category, Review
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
 
 
 """
@@ -451,3 +457,284 @@ class SupplierDetail(APIView):
         supplier = self.get_object(pk)
         supplier.delete()
         return Response({"detail": "Supplier deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+    
+
+class MenuList(APIView):
+    """
+    Endpoint to handle both fetching all menus and creating a new menu.
+    Only admins can fetch all menus; any user can create a menu.
+    """
+
+    allowed_methods = ['GET', 'POST']
+    parser_classes = [MultiPartParser, FormParser]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return MenuCreateSerializer
+        return MenuSerializer
+
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsAdmin()]
+        return [IsAuthenticated]
+
+    def get(self, request):
+        """
+        Handle GET request to fetch all menu items.
+        """
+        category_id = request.query_params.get('category_id', None)
+        if category_id is not None:
+            try:
+                category =  Category.objects.get(id=category_id)
+                items = category.items.all()
+            except Category.DoesNotExist:
+                items = []
+        else:
+            items = Menu.objects.all()
+
+        serializer = self.get_serializer_class()(items, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """
+        Handle POST request to create a new menu.
+        """
+        serializer = self.get_serializer_class()(data=request.data)
+        if serializer.is_valid():
+            menu = serializer.save(commit=False)
+            image_file = request.FILES.get('image')
+            if image_file:
+                img = Image.open(image_file)
+                size = (300, 300)
+                img.thumbnail(size)
+
+                if img.mode == 'RGBA':
+                    img = img.convert('RGB')
+
+                thumb_io = BytesIO()
+                img.save(thumb_io, format='JPEG')
+                thumb_io.seek(0)
+
+                # Save the thumbnail to the model
+                menu.thumbnail.save(f"thumb_{image_file.name}", ContentFile(thumb_io.read()), save=False)
+            menu.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class MenuDetail(APIView):
+    """
+    Retrieve, update, or delete a menu instance.
+    """
+
+    allowed_methods = ['GET', 'PUT', 'DELETE']
+    parser_classes = [MultiPartParser, FormParser]
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'DELETE']:
+            return [IsAdmin()]
+        return [IsAuthenticated()]
+
+    def get_serializer_class(self):
+        if self.request.method == 'PUT':
+            return MenuUpdateSerializer
+        return MenuSerializer
+
+    def get_object(self, pk):
+        return get_object_or_404(Menu, pk=pk)
+
+    def get(self, request, pk):
+        """
+        Retrieve menu details.
+        """
+        item = self.get_object(pk)
+        serializer = self.get_serializer_class()(item)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        """
+        Update a menu item.
+        """
+        item = self.get_object(pk)
+        serializer = self.get_serializer_class()(item, data=request.data)
+        if serializer.is_valid():
+            image_file = request.FILES.get('image')
+            if image_file:
+                img = Image.open(image_file)
+                size = (300, 300)
+                img.thumbnail(size)
+
+                if img.mode == 'RGBA':
+                    img = img.convert('RGB')
+
+                thumb_io = BytesIO()
+                img.save(thumb_io, format='JPEG')
+                thumb_io.seek(0)
+
+                item.thumbnail.save(f"thumb_{image_file.name}", ContentFile(thumb_io.read()), save=False)
+
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        """
+        Delete a menu item.
+        """
+        item = self.get_object(pk)
+        item.delete()
+        return Response({"detail": "Menu item deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
+
+class CategoryList(APIView):
+    """
+    Handles listing all categories and creating a new category.
+    """
+
+    allowed_methods = ['GET', 'POST']
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return CategorySerializer
+        return CategoryCreateSerializer
+
+    def get(self, request):
+        """
+        Fetch all categories.
+        """
+        categories = Category.objects.all()
+        serializer = self.get_serializer_class()(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """
+        Create a new category.
+        """
+        serializer = self.get_serializer_class()(data=request.data)
+        if serializer.is_valid():
+            category = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CategoryDetail(APIView):
+    """
+    Handles retrieving, updating, and deleting a single category.
+    """
+    allowed_methods = ['GET', 'DELETE']
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+    def get_object(self, pk):
+        return get_object_or_404(Category, pk=pk)
+    
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return CategorySerializer
+        return None
+
+    def get(self, request, pk):
+        """
+        Retrieve a single category.
+        """
+        category = self.get_object(pk)
+        serializer = self.get_serializer_class()(category)
+        return Response(serializer.data)
+    
+    def delete(self, request, pk):
+        """
+        Delete a category.
+        """
+        category = self.get_object(pk)
+        category.delete()
+        return Response({"detail": "Category deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
+
+class ReviewList(APIView):
+    """
+    Endpoint for fetching and creating reviews
+    """
+
+    allowed_methods = ['GET', 'POST']
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ReviewSerializer
+        return ReviewCreateSerializer
+    
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsAdmin()]
+        return [IsCustomer()]
+    
+    def get(self, request):
+        menu_id = request.query_params.get('menu_id', None)
+        if not menu_id:
+            return Response({"detail": "Menu id is required"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            menu_item = Menu.objects.get(id=menu_id)
+        except Menu.DoesNotExist:
+            return Response({"detail": "Menu item not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        reviews = menu_item.reviews.all()
+        serializer = self.get_serializer_class()(reviews, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = self.get_serializer_class()(data=request.data)
+        if serializer.is_valid():
+            review = serializer.save(commit=False)
+            review.reviewer = self.request.user
+            review.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class ReviewDetail(APIView):
+    """
+    Handles retrieving, updating, and deleting a single review.
+    """
+    allowed_methods = ['GET', 'PUT', 'DELETE']
+    
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'DELETE']:
+            return [IsCustomer()]
+        return [IsAuthenticated()]
+
+    def get_object(self, pk):
+        return get_object_or_404(Review, pk=pk)
+    
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ReviewSerializer
+        elif self.request.method == 'PUT':
+            return ReviewUpdateSerializer
+        return None
+
+    def get(self, request, pk):
+        """
+        Retrieve a single review.
+        """
+        review = self.get_object(pk)
+        serializer = self.get_serializer_class()(review)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        review = self.get_object(pk)
+        if review.reviewer.id != self.request.user.id:
+            return Response({"detail": "Access Denied"}, status=status.HTTP_401_UNAUTHORIZED)
+        serializer = self.get_serializer_class()(review, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        """
+        Delete a review.
+        """
+        review = self.get_object(pk)
+        if review.reviewer.id != self.request.user.id:
+            return Response({"detail": "Access Denied"}, status=status.HTTP_401_UNAUTHORIZED)
+        review.delete()
+        return Response({"detail": "Review deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
